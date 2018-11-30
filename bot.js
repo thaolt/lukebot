fdd53c47a5c112c52f93b52c33c7ff1897b1b54e
thaolt <thaolt@songphi.com>
2018-11-30 22:31:08 +0700

    added vntk

--- bot.js
+++ bot.js
@@ -1,6 +1,28 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 const moment = require('moment');
+const vntk = require('vntk');
+var posTag = vntk.posTag();
+// var dictionary = vntk.dictionary();
+// var ner = vntk.ner();
+
+var classifier = new vntk.BayesClassifier();
+
+classifier.addDocument('khi nào trận chiến đã kết thúc?', 'when');
+classifier.addDocument('tàu rời đi lúc mấy giờ?', 'when');
+classifier.addDocument('trận đấu diễn ra vào thời gian nào?', 'when');
+classifier.addDocument('anh ấy rời đi vào lúc mấy giờ?', 'when');
+classifier.addDocument('bao giờ thì đến lễ hội hóa trang?', 'when');
+classifier.addDocument('ai phát hiện ra điện ?', 'who');
+classifier.addDocument('người sáng lập ra microsoft là ai?', 'who');
+classifier.addDocument('ai kiếm được tiền của họ một cách chăm chỉ ?', 'who');
+classifier.addDocument('người phát minh tạo ra.', 'who');
+classifier.addDocument('gia đình bạn gồm những ai?', 'who');
+classifier.addDocument('làm thế nào để bẻ khóa?', 'how');
+classifier.addDocument('Việt Nam nằm ở đâu?', 'where');
+classifier.addDocument('Hà Nội kế tỉnh nào?', 'where');
+
+classifier.train();
 
 const { ActivityTypes } = require('botbuilder');
 
@@ -28,30 +50,37 @@ class MyBot {
             // read from state.
             // let count = await this.countProperty.get(turnContext);
             // count = count === undefined ? 1 : ++count;
-            let command = turnContext.activity.text.toLowerCase();
-            command = command.replace(/^.*\s/, '');
-            command = command.trim();
-            let message = "";
-            if (command == "tet") {
-                const remain_days = - moment().diff('2019-02-05', 'days');
-                message = `Còn có ${remain_days} ngày nữa là tới tết à. Cứ từ từ. Tết này không có thưởng đâu Danh ơi.`;
+            let message = turnContext.activity.text;
+            message = message.replace(/^.*?\s/, '');
+            message = message.trim();
+            let response = '';
+            if (message.toLowerCase() === 'tet') {
+                const remainDays = -moment().diff('2019-02-05', 'days');
+                response = `Còn có ${ remainDays } ngày nữa là tới tết à. Cứ từ từ. Tết này không có thưởng đâu Danh ơi.`;
 
-                await turnContext.sendActivity(message);
+                await turnContext.sendActivity(response);
             }
-            if (command == "noel" || command == "christmas") {
-                const remain_days = - moment().diff('2018-12-24', 'days');
-                message = `Còn ${remain_days} ngày nữa là đến Noel rồi. Bạn dã có người yêu chưa. Khẩn trương lên.`;
+            if (message.toLowerCase() === 'noel' || message.toLowerCase() === 'christmas') {
+                const remainDays = -moment().diff('2018-12-24', 'days');
+                response = `Còn ${ remainDays } ngày nữa là đến Noel rồi. Bạn dã có người yêu chưa. Khẩn trương lên.`;
 
-                await turnContext.sendActivity(message);
+                await turnContext.sendActivity(response);
             }
-            if (command == "valentin") {
-                const remain_days = - moment().diff('2019-02-14', 'days');
-                message = `Còn ${remain_days} ngày nữa là đến Valentin rồi. Mình vẫn cô đơn. kaka.`;
+            if (message.toLowerCase() === 'valentin') {
+                const remainDays = -moment().diff('2019-02-14', 'days');
+                response = `Còn ${ remainDays } ngày nữa là đến Valentin rồi. Mình vẫn cô đơn. kaka.`;
 
-                await turnContext.sendActivity(message);
+                await turnContext.sendActivity(response);
             }
             // increment and set turn counter.
             // await this.countProperty.set(turnContext, count);
+            if (response === '') {
+                // console.log(posTag.tag(message));
+                await turnContext.sendActivity('POS: ' + posTag.tag(message, 'text'));
+                // console.log(ner.tag(message));
+                // await turnContext.sendActivity('NER: ' + ner.tag(message, 'text'));
+                await turnContext.sendActivity('CLASSIFY: ' + classifier.classify(message));
+            }
         } else {
             // await turnContext.sendActivity(`[${turnContext.activity.type} event detected]`);
         }
